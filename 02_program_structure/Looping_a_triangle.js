4195ec4b325243e3abc4ba724836ca2f54b65552
Iskandar Gabdrakhmanov <faruk.iskandar@yandex.ru>
2016-12-26 17:56:06 +0300

    Added solution to the problems from Chapter 2.

---
+++ 02_program_structure/Looping_a_triangle.js
@@ -0,0 +1,12 @@
+/*
+	Write a loop that makes seven calls to console.log to output the following triangle:
+	#
+	##
+	###
+	####
+	#####
+	######
+	#######
+*/
+for (var line = "#"; line.length <= 7; line += "#")
+	console.log(line);
