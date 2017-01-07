ecba3ce86b67085a7a9c829bc34e38d1ff913d45
Iskandar Gabdrakhmanov <faruk.iskandar@yandex.ru>
2017-01-07 22:43:10 +0300

    Update BeanCounting.js

--- 03_functions/BeanCounting.js
+++ 03_functions/BeanCounting.js
@@ -1,25 +1,40 @@
-/*
-	Write a function countBs that takes a string as its only argument
-	and returns a number that indicates how many uppercase “B” characters are in the string.
+/**
+ * @file Write a function countBs that takes a string as its only argument
+ * and returns a number that indicates how many uppercase “B” characters are in the string.
+ * Next, write a function called countChar that behaves like countBs,
+ * except it takes a second argument that indicates the character
+ * that is to be counted (rather than counting only uppercase “B” characters).
+ * Rewrite countBs to make use of this new function.
+ */
 
-	Next, write a function called countChar that behaves like countBs,
-	except it takes a second argument that indicates the character
-	that is to be counted (rather than counting only uppercase “B” characters).
-	Rewrite countBs to make use of this new function.
-*/
-
+/**
+ * Counts the amount of B chars in string.
+ * @param {string} str - input string.
+ * @returns the amount of B chars.
+ */
 function countBs(str){
 	return countChar(str, "B");
 }
 
+/**
+ * Counts the amount of @ref ch in @str.
+ * @param {string} str - input string.
+ * @param {string} ch - char to count.
+ * @returns the amount of @ref ch in @ref str.
+ */
 function countChar(str, ch){
 	var chCounter = 0;
-	for (var strIdx = 0; strIdx < str.length; strIdx++){
+	for (var strIdx = 0; strIdx < str.length; strIdx++) {
 		chCounter += str.charAt(strIdx) == ch ? 1 : 0;
 	}
 	return chCounter;
 }
 
+/**
+ * Fails if condition is false.
+ * @param {boolean} condition - condition to be checked.
+ * @param {string} message - exception message.
+ */
 function assert(condition, message) {
     if (!condition) {
         throw message || "Assertion failed";
