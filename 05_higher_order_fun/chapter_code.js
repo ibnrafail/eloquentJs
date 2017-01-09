47ee974736a132ad6eef849aa3c8924477ad42dd
Iskandar Gabdrakhmanov <faruk.iskandar@yandex.ru>
2017-01-09 12:40:55 +0300

    Composability ex. added.

--- 05_higher_order_fun/chapter_code.js
+++ 05_higher_order_fun/chapter_code.js
@@ -162,3 +162,25 @@ console.log(ancestry.reduce(function(min, cur) {
 	if (cur.born < min.born) return cur;
 	else return min;
 }));
+
+/* ================================ */
+console.log("Composability.");
+
+var min = ancestry[0];
+for (var i = 0; i < ancestry.length; i++) {
+	var cur = ancestry[i];
+	if (cur.born < min.born) min = cur;
+}
+console.log(min);
+
+function average(array) {
+	function plus(a, b) { return a + b; }
+	return array.reduce(plus) / array.length;
+}
+
+function age(p) { return p.died - p.born; }
+function male(p) { return p.sex == "m"; }
+function female(p) { return p.sex == "f"; }
+
+console.log(average(ancestry.filter(male).map(age)));
+console.log(average(ancestry.filter(female).map(age)));
